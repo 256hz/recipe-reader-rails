7529e0d3cb895d0e04e2920d538343c41db8ec18
Abe Dolinger <a.d@256hz.com>
2019-06-11 16:51:45 -0700

    trying stuff

--- app/api-tests/google-text-to-speech/TTS-test-1.js
+++ app/api-tests/google-text-to-speech/TTS-test-1.js
@@ -1,30 +1,30 @@
-// Imports the Google Cloud client library
-const textToSpeech = require('@google-cloud/text-to-speech');
+// // Imports the Google Cloud client library
+// const textToSpeech = require('@google-cloud/text-to-speech');
 
-// Import other required libraries
-const fs = require('fs');
-const util = require('util');
-async function main() {
-  // Creates a client
-  const client = new textToSpeech.TextToSpeechClient();
+// // Import other required libraries
+// const fs = require('fs');
+// const util = require('util');
+// async function main() {
+//   // Creates a client
+//   const client = new textToSpeech.TextToSpeechClient();
 
-  // The text to synthesize
-  const text = 'Abe says Hello, world!';
-  console.log({text})
+//   // The text to synthesize
+//   const text = 'Abe says Hello, world!';
+//   console.log({text})
 
-  // Construct the request
-  const request = {
-    input: {text: text},
-    // Select the language and SSML Voice Gender (optional)
-    voice: {languageCode: 'en-US', ssmlGender: 'NEUTRAL'},
-    // Select the type of audio encoding
-    audioConfig: {audioEncoding: 'MP3'},
-  };
+//   // Construct the request
+//   const request = {
+//     input: {text: text},
+//     // Select the language and SSML Voice Gender (optional)
+//     voice: {languageCode: 'en-US', ssmlGender: 'NEUTRAL'},
+//     // Select the type of audio encoding
+//     audioConfig: {audioEncoding: 'MP3'},
+//   };
 
-  // Performs the Text-to-Speech request
-  const [response] = await client.synthesizeSpeech(request);
-  // Write the binary audio content to a local file
-  const writeFile = util.promisify(fs.writeFile);
-  await writeFile('./output.mp3', response.audioContent, 'binary');
-  console.log('Audio content written to file: output.mp3');
-}
+//   // Performs the Text-to-Speech request
+//   const [response] = await client.synthesizeSpeech(request);
+//   // Write the binary audio content to a local file
+//   const writeFile = util.promisify(fs.writeFile);
+//   await writeFile('./output.mp3', response.audioContent, 'binary');
+//   console.log('Audio content written to file: output.mp3');
+// }
