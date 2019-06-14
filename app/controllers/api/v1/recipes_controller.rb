e4ef379ff90b8431960df8907d2c7efff1d203bc
Abe Dolinger <a.d@256hz.com>
2019-06-14 13:21:21 -0700

    added sort by method for returning recipe steps

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -7,10 +7,10 @@ class Api::V1::RecipesController < ApplicationController
     def show
         @recipe = Recipe.all.find_by(spoon_id: params[:id])
         if @recipe
-            render json: @recipe.steps
+            render json: @recipe.steps.sort_by(&:step_no)
         else
             @recipe = Fetcher.get_recipe(params[:id])
-            render json: @recipe.steps
+            render json: @recipe.steps.sort_by(&:step_no)
         end
     end
 end
