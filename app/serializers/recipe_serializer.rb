c52d2879e95a06636f205e74edb709ba84320424
Abe Dolinger <a.d@256hz.com>
2019-06-18 16:49:43 -0700

    added source_url to :recipes, looked for dupes in Ingredient creation

--- app/serializers/recipe_serializer.rb
+++ app/serializers/recipe_serializer.rb
@@ -1,9 +1,18 @@
 class RecipeSerializer < ActiveModel::Serializer
-  attributes :id, :spoon_id,
-    :title, :image_url,
-    :is_vegetarian, :is_vegan,
-    :ready_in_minutes, :servings,
-    :source_name, :likes,
-    :cuisines, :dish_types,
-    :ingredients, :steps
+  attributes 
+    :cuisines, 
+    :dish_types,
+    :ingredients, 
+    :id, 
+    :image_url,
+    :is_vegetarian, 
+    :is_vegan,
+    :likes,
+    :ready_in_minutes, 
+    :servings,
+    :source_name, 
+    :steps
+    :source_url,
+    :spoon_id,
+    :title, 
 end
