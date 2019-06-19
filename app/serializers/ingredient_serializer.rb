36032967252b24cbd76cd5c4d0c8e98273a4c314
Abe Dolinger <a.d@256hz.com>
2019-06-19 13:36:21 -0700

    TTS works locally

--- app/serializers/ingredient_serializer.rb
+++ app/serializers/ingredient_serializer.rb
@@ -1,6 +1,11 @@
 class IngredientSerializer < ActiveModel::Serializer
-  attributes :id, :spoon_id, :name,
-    :metric_amount, :metric_unit, 
-    :us_amount, :us_unit,
-    :image_url, :orig_string
+  attributes :id, 
+  :spoon_id, 
+  :name,
+  :metric_amount, 
+  :metric_unit, 
+  :us_amount, 
+  :us_unit,
+  :image_url, 
+  :orig_string
 end
