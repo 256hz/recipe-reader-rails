215a8a8f89aa605aa3d162a5fbbccb88abbf5c8d
Abe Dolinger <a.d@256hz.com>
2019-08-23 16:35:05 -0700

    about 60% on comments

--- app/models/fetcher.rb
+++ app/models/fetcher.rb
@@ -1,24 +1,25 @@
 require 'httparty'
 
 # Calls the Spoonacular API for recipe search and recipe steps.
-# SEARCH: #request_search, #search
+# Order of operations:
+# SEARCH: search, request_search
 # STEPS:
-request_recipe
-get_recipe
-  create_recipe
-  create_ingredients
-  create_steps
-  associate_step_ingredients
-
-  
-get_recipe(id)
-  
+#   get_recipe
+#     request_recipe
+#     create_recipe
+#     create_ingredients
+#     create_steps
+#       get_spoon_ids
+#     associate_step_ingredients
 class Fetcher
   def self.key
+    # For returning the API key from memory
     ENV.fetch('SPOONACULAR_API_KEY')
   end
 
   def self.request_search(query)
+    # Performs Spoonacular(S11r)'s 'complex recipe' search, since simple search seems to be down.
+    # Returns JSON results that include complex instructions, i.e., step associations.
     HTTParty.get(
       'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/searchComplex' + 
       "?query=#{query}&instructionsRequired=true",
@@ -28,6 +29,7 @@ class Fetcher
   end
 
   def self.search(query)
+    # Searches for recipes from S11r and returns them in a @results hash.  
     response = request_search(query)
     if response
       @results = {}
@@ -37,24 +39,21 @@ class Fetcher
                                    readyInMinutes: res['readyInMinutes'] }
       end
     else
-      @results = { '0001': 'No recipes found' }
+      @results = { 'None' => 'No recipes found' }
     end
     # puts @results
     @results
   end
 
-  def self.request_recipe(id)
-    HTTParty.get('https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/' +
-                 "recipes/#{id}/information",
-                 headers: { 'X-RapidAPI-Host' => 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com',
-                            'X-RapidAPI-Key' => key }
-                )
-  end
-
   def self.get_recipe(id)
+    # Runner method for getting a recipe.
+    # - gets the recipe from S11r
+    # - creates a Recipe to associate everything with
+    # - creates Ingredients & RecipeIngredients joins for this recipe
+    # - creates Steps & RecipeSteps joins
+    # - creates StepIngredient joins.
     response = request_recipe(id)
-    puts response
-
+    # puts response
     @recipe = create_recipe(response)
     @ingredients = create_ingredients(response, @recipe)
     @steps = create_steps(response, @recipe.id, @ingredients)
@@ -62,7 +61,16 @@ class Fetcher
     @recipe
   end
 
+  def self.request_recipe(id)
+    # Return a single recipe's instructions and equipment in JSON.
+    HTTParty.get('https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/' +
+                 "recipes/#{id}/information",
+                 headers: { 'X-RapidAPI-Host' => 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com',
+                            'X-RapidAPI-Key' => key })
+  end
+
   def self.create_recipe(response)
+    # Create a new Recipe in the DB.
     @recipe = Recipe.create(cuisines: response['cuisines'],
                             dish_types: response['dishTypes'],
                             image_url: response['image'],
@@ -79,14 +87,16 @@ class Fetcher
   end
 
   def self.create_ingredients(response, recipe)
+    # Makes Ingredients if they don't already exist.  Associates all with the Recipe.
+    # 
     ingredients = []
     response['extendedIngredients'].each do |ingred|
       # puts "ingredient:", ingred['name'], 'spoon_id:', ingred['id']
       @ingred = Ingredient.all.find_by(spoon_id: ingred['id'])
       if @ingred.nil? || @recipe.ingredients.map(&:spoon_id).include?(ingred['id'])
-        name = filter_name(ingred['name'])
         @ingred = Ingredient.create!(spoon_id: ingred['id'],
-                                     name: name,
+                                     # see filter_name comments below
+                                     name: filter_name(ingred['name']),
                                      orig_string: ingred['originalString'],
                                      metric_amount: round_to_fraction(ingred['measures']['metric']['amount']),
                                      metric_unit: ingred['measures']['metric']['unitShort'],
@@ -94,19 +104,23 @@ class Fetcher
                                      us_unit: ingred['measures']['us']['unitShort'],
                                      image_url: ingred['image'])
       end
-      ri = RecipeIngredient.find_or_create_by(recipe_id: recipe.id,
-                                              ingredient_id: @ingred.id)
-      puts 'created recipe_ingredient id:', ri.id
+      ri = RecipeIngredient.find_or_create_by(recipe_id: recipe.id, ingredient_id: @ingred.id)
+      # puts 'created recipe_ingredient id:', ri.id
       ingredients.push(@ingred)
     end
     ingredients
   end
 
   def self.round_to_fraction(float, denominator = 4)
+    # The API often returns amounts in long floats, probably as a result of
+    # automated conversion to/from metric.  This rounds to the nearest quarter,
+    # unless another denominator is specified.
     (float * denominator).round.to_f / denominator
   end
 
   def self.filter_name(name)
+    # This is a janky way to remove very common words from the Ingredient name,
+    # which helps prevent false positives when associating StepIngredients later.
     words_to_strip = %w[fresh green red]
     words_to_strip.each do |word|
       name = name.split.filter { |el| el != word }.join(' ')
@@ -114,14 +128,36 @@ class Fetcher
     name
   end
 
+  def self.create_steps(response, recipe_id, ingredients)
+    # Creates Steps from the response and associates ingredents & recipe.
+    # If the step has equipment, it creates it below in create_equipment.
+    # I don't love that the create_equipment call is nested in this function,
+    # good refactor target.
+    steps = []
+    response['analyzedInstructions'][0]['steps'].each do |step|
+      # puts "Step ingredients:", step['ingredients']
+      # puts "Step ingredient Spoon IDs:", step['ingredients'].map{|i| i['id'].to_s}
+      step_spoon_ids = get_spoon_ids(step['step'], ingredients)
+      @step = Step.create!(recipe_id: recipe_id,
+                           step_no: step['number'],
+                           text: step['step'],
+                           spoon_ids: step_spoon_ids)
+      create_equipment(step['equipment'], recipe_id, @step.id) if step['equipment']
+      steps.push(@step)
+    end
+    steps
+  end
+
   def self.create_equipment(equipment, recipe_id, step_id)
+    # Takes a list of equipment from the Step created above, creates each one,
+    # and joins it with the step and recipe.
     # puts 'creating equipment'
     # puts equipment, recipe_id, step_id
     equipment.each do |item|
       # puts 'item:', item
       @equip = Equipment.find_by(spoon_id: item['id'])
       if @equip.nil?
-        puts 'equipment not found, creating:', item['id'], item['name'], item['image']
+        # puts 'equipment not found, creating:', item['id'], item['name'], item['image']
         @equip = Equipment.create!(spoon_id: item['id'],
                                    name: item['name'],
                                    image_url: item['image'])
@@ -131,39 +167,25 @@ class Fetcher
       StepEquipment.create!(equipment_id: @equip.id, step_id: step_id)
     end
   end
-
-  def self.create_steps(response, id, ingredients)
-    steps = []
-    # byebug
-    response['analyzedInstructions'][0]['steps'].each do |step|
-      # puts "Step ingredients:", step['ingredients']
-      # puts "Step ingredient Spoon IDs:", step['ingredients'].map{|i| i['id'].to_s}
-      step_spoon_ids = get_spoon_ids(step['step'], ingredients)
-      @step = Step.create!(recipe_id: id,
-                           step_no: step['number'],
-                           text: step['step'],
-                           spoon_ids: step_spoon_ids)
-      create_equipment(step['equipment'], id, @step.id) if step['equipment']
-      steps.push(@step)
-    end
-    steps
-  end
-
+  
   def self.get_spoon_ids(text, ingredients)
+    # Sometimes, the API returns different ingredients in the step than it does in
+    # the Recipe's extendedIngredients field (e.g., cherry jam vs. raspberry).
+    # This searches the ingredients list for names that match, i.e., cherry jam
+    # would match rasbperry jam due to the common word 'jam'.  Very janky that
+    # this would cause problems with multiple types of the same ingredient, but
+    # practically hasn't been a huge problem thus far.
     spoon_ids = []
-    puts "Searching: #{text}"
+    # puts "Searching: #{text}"
     ingredients.each do |i|
+      # puts "Looking for: #{i.name}"
+      spoon_ids << i.spoon_id if text.downcase.include?(i.name)
+      
+      # puts "found, inserting: #{i.name}, #{i.spoon_id}"
       split_name = i.name.downcase.split
-      # puts "Looking for: #{i.name} or #{split_name}"
-      if text.downcase.include?(i.name)
-        spoon_ids << i.spoon_id
-        # puts "found, inserting: #{i.name}, #{i.spoon_id}"
-      end
       split_name.each do |word|
-        if text.include?(word) && !spoon_ids.include?(i.spoon_id)
-          spoon_ids << i.spoon_id
-          puts "found, inserting: #{i.name}, #{i.spoon_id}"
-        end
+        spoon_ids << i.spoon_id if text.include?(word) && !spoon_ids.include?(i.spoon_id)
+        # puts "found, inserting: #{i.name}, #{i.spoon_id}"
       end
     end
     # puts "spoon_ids: #{spoon_ids}"
@@ -171,7 +193,7 @@ class Fetcher
   end
 
   def self.associate_step_ingredients(steps)
-    puts "Associating steps with ingredients"
+    puts 'Associating steps with ingredients'
     steps.each do |step_i|
       next if step_i['spoon_ids'] == []
 
@@ -184,5 +206,4 @@ class Fetcher
       end
     end
   end
-
 end
